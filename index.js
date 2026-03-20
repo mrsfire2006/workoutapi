4dbcbdd78fa42424a76857e6e1a5858eab555911
Mohamed Sarhan <mrs.mohamedsarhan2024@gmail.com>
2026-03-20 14:53:04 +0200

    Final adjustments for Vercel

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors()); // يمكنك تحديد رابط الـ Frontend هنا لاحقاً للأمان
 
-// الاتصال بـ MongoDB
+
 mongoose.connect(process.env.MONG_URI)
-  .then(() => {
-    // تشغيل السيرفر فقط بعد نجاح الاتصال بالقاعدة
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT} and connected to DB`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB Connection Error:", err);
-  });
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
+// الاتصال بـ MongoDB
+// mongoose.connect(process.env.MONG_URI)
+//   .then(() => {
+//     // تشغيل السيرفر فقط بعد نجاح الاتصال بالقاعدة
+//     const PORT = process.env.PORT || 5000;
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT} and connected to DB`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB Connection Error:", err);
+//   });
 
 // تعريف المسارات مباشرة (بدون تعقيدات Netlify)
 app.get("/", (req, res) => {
@@ -33,3 +37,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/users", userRoutes);
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+module.exports = app;
